4a99d8f3742ae269bcc94996601e4e087902a1aa
BuildTools <bmceachnie@student.tgm.ac.at>
2022-03-08 23:06:15 +0100

    hover anims

--- lists/lib/routes/lists/list.dart
+++ lists/lib/routes/lists/list.dart
@@ -4,7 +4,6 @@ import 'package:lists/assets/assets.dart';
 import 'package:lists/db/collection.dart';
 import 'package:lists/helpers/collection.dart';
 import 'package:lists/helpers/dialogs.dart';
-import 'package:lists/helpers/styles/styles.dart';
 import 'package:lists/widgets/context_menu.dart';
 import 'package:lists/widgets/tooltip_icon_button.dart';
 import 'package:nekolib_ui/core.dart';
@@ -23,88 +22,93 @@ class CollectionTile extends StatefulWidget {
 class _CollectionTileState extends State<CollectionTile> {
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: ContextMenuRegion(
-        contextMenu: ContextMenu(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TooltipIconButton(
-                tooltip: "Edit ${widget.collection.name}",
-                onPressed: () => updateCollection(context, widget.collection),
-                icon: FluentIcons.ic_fluent_edit_24_filled,
-              ),
-              TooltipIconButton(
-                tooltip: "Delete ${widget.collection.name}",
-                onPressed: () => deleteCollection(context, widget.collection),
-                icon: FluentIcons.ic_fluent_delete_24_filled,
-              ),
-            ],
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () => showPasswordDialog(context, widget.collection),
-          child: Container(
-            height: 260,
-            width: 200,
-            padding: EdgeInsets.all(15),
-            margin: EdgeInsets.only(bottom: 8),
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(8.0),
-              boxShadow: kElevationToShadow[2],
+    return ContextMenuRegion(
+      contextMenu: ContextMenu(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TooltipIconButton(
+              tooltip: "Edit ${widget.collection.name}",
+              onPressed: () => updateCollection(context, widget.collection),
+              icon: FluentIcons.ic_fluent_edit_24_filled,
             ),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            TooltipIconButton(
+              tooltip: "Delete ${widget.collection.name}",
+              onPressed: () => deleteCollection(context, widget.collection),
+              icon: FluentIcons.ic_fluent_delete_24_filled,
+            ),
+          ],
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () => showPasswordDialog(context, widget.collection),
+        child: SizedBox(
+          height: 260,
+          width: 200,
+          child: OffsetOnHover(
+            duration: FluentTheme.of(context).fastAnimationDuration,
+            curve: FluentTheme.of(context).animationCurve,
+            // scale: 1.025,
+            offset: Offset(0, -5),
+            child: Container(
+              padding: EdgeInsets.all(15),
+              margin: EdgeInsets.only(bottom: 8),
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: kElevationToShadow[2],
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: CollectionTile.iconSize,
+                          height: CollectionTile.iconSize,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4.0),
+                            child: widget.collection.hasIcon
+                                ? Image.memory(
+                                    widget.collection.icon,
+                                  )
+                                : Image.asset(
+                                    img_default_list_icon,
+                                    color: textColor,
+                                  ),
+                          ),
+                        ),
+                        NcSpacing.xl(),
+                        SizedBox(
+                          width: double.infinity,
+                          child: NcCaptionText(
+                            widget.collection.name,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  NcSpacing.large(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        width: CollectionTile.iconSize,
-                        height: CollectionTile.iconSize,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4.0),
-                          child: widget.collection.hasIcon
-                              ? Image.memory(
-                                  widget.collection.icon,
-                                )
-                              : Image.asset(
-                                  img_default_list_icon,
-                                  color: textColor,
-                                ),
+                      NcBodyText('${widget.collection.length} item${widget.collection.length != 1 ? "s" : ""}'),
+                      if (widget.collection.isProtected) NcSpacing.small(),
+                      if (widget.collection.isProtected)
+                        Tooltip(
+                          message: 'This list is password protected',
+                          child: Icon(
+                            FluentIcons.ic_fluent_shield_24_regular,
+                            color: textColor,
+                            size: 18,
+                          ),
                         ),
-                      ),
-                      NcSpacing.xl(),
-                      SizedBox(
-                        width: double.infinity,
-                        child: NcCaptionText(
-                          widget.collection.name,
-                          fontSize: 15,
-                        ),
-                      ),
                     ],
                   ),
-                ),
-                NcSpacing.large(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    NcBodyText('${widget.collection.length} item${widget.collection.length != 1 ? "s" : ""}'),
-                    if (widget.collection.isProtected) NcSpacing.small(),
-                    if (widget.collection.isProtected)
-                      Tooltip(
-                        message: 'This list is password protected',
-                        child: Icon(
-                          FluentIcons.ic_fluent_shield_24_regular,
-                          color: textColor,
-                          size: 18,
-                        ),
-                      ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
