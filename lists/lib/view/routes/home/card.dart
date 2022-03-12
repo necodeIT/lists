9ae60697ecaf9a8a48119197c0262dc98526b4a1
BuildTools <bmceachnie@student.tgm.ac.at>
2022-03-12 01:27:01 +0100

    home screen hover animations + new card

--- lists/lib/view/routes/home/card.dart
+++ lists/lib/view/routes/home/card.dart
@@ -1,6 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:lists/view/widgets/info_box_container.dart';
 import 'package:nekolib_ui/core.dart';
+import 'package:nekolib_ui/utils.dart';
 
 class HomeCard extends StatelessWidget {
   const HomeCard({Key? key, required this.onTap, required this.title, required this.icon}) : super(key: key);
@@ -11,24 +12,27 @@ class HomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
+    return GestureDetector(
+      onTap: onTap,
+      child: OffsetOnHover(
+        duration: FluentTheme.of(context).fastAnimationDuration,
+        curve: FluentTheme.of(context).animationCurve,
+        offset: Offset(0, -5),
+        child: SizedBox(
           width: 250,
           height: 300,
-          decoration: InfoBoxContainer.decoration(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, size: 60),
-              NcSpacing.medium(),
-              NcBodyText(
-                title,
-                fontSize: 20,
-              ),
-            ],
+          child: Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, size: 60),
+                NcSpacing.medium(),
+                NcBodyText(
+                  title,
+                  fontSize: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
